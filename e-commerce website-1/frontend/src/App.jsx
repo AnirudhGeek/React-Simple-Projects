ab0e1b029053d468fb4b42e40041820f86cd48ee
Anirudh Raturi <annigeek@gmail.com>
2025-07-30 15:36:41 +0530

    updated app.js

--- e-commerce website-1/frontend/src/App.jsx
+++ e-commerce website-1/frontend/src/App.jsx
@@ -13,6 +13,8 @@ import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
 import { Route } from "react-router-dom";
 
+//laptop's fans got damaged;)
+
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
