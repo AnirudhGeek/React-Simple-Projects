2058b2369428f75c9bec7698603cc47b9d735e15
Anirudh Raturi <annigeek@gmail.com>
2025-08-06 18:09:52 +0530

    Product page completed

--- e-commerce website-1/frontend/src/App.jsx
+++ e-commerce website-1/frontend/src/App.jsx
@@ -13,12 +13,14 @@ import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
 import { Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
 
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar />
+      <SearchBar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
--- e-commerce website-1/frontend/src/context/ShopContext.jsx
+++ e-commerce website-1/frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext = createContext();
@@ -6,18 +6,22 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
 
   // whenever we add variable state variable or a function within this value object then we can access it in any component using the contextAPI
   const value = {
     products,
     currency,
     delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
   };
 
   return (
-    <ShopContext.Provider value={value}>
-        {props.children}
-    </ShopContext.Provider>
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
   );
 };
 
