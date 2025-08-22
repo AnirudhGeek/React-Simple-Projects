7e22854c3ea1d00166f1d05b1ca4e3bb85502e5e
Anirudh Raturi <annigeek@gmail.com>
2025-08-23 00:13:56 +0530

    user order data for frontend

---
+++ e-commerce website-1/backend/controllers/orderController.js
@@ -0,0 +1,76 @@
+import orderModel from "../models/OrderModel.js";
+import userModel from "../models/userModel.js";
+import mongoose from "mongoose";
+
+//placing orders using COD method
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+    const orderData = {
+        userId,
+        items,
+        address,
+        amount,
+        paymentMethod:"COD",
+        payment: false,
+        date:Date.now()
+    }
+
+    const newOrder = new orderModel(orderData)
+    await newOrder.save()
+
+    //so now whenver the order data is placed we have to clear up the cartData of this user 
+    await userModel.findByIdAndUpdate(userId,{cartData:{}})
+
+    res.json({
+        success : true,
+        message: "Order Placed"
+    })
+  } catch (error) {
+    console.log(error)
+    res.json({
+        success : false,
+        message : error.message
+    })
+  }
+};
+
+//placing orders using Stripe method
+const placeOrderStripe = async (req, res) => {};
+
+//placing orders using Razorpay method
+const placeOrderRazorpay = async (req, res) => {};
+
+//All orders data for Admin Panel
+const allOrders = async (req, res) => {
+};
+
+//User Order data for frontend
+const userOrders = async (req, res) => {
+    try {
+        const {userId} = req.body
+        const orders = await orderModel.find({userId})
+        res.json({
+            success : true,
+            orders
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success : false,
+            message : error.message
+        })
+    }
+};
+
+//update order status from Admin Panel
+const updateStatus = async (req, res) => {};
+
+export {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+};
--- e-commerce website-1/frontend/src/pages/PlaceOrder.jsx
+++ e-commerce website-1/frontend/src/pages/PlaceOrder.jsx
@@ -3,13 +3,96 @@ import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
 import { assets } from "../assets/frontend_assets/assets";
 import { ShopContext } from "../context/ShopContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const PlaceOrder = () => {
   const [method, setMethod] = useState("cod");
-  const {navigate} = useContext(ShopContext)
+  const {
+    navigate,
+    backendUrl,
+    token,
+    cartItems,
+    setCartItems,
+    getCartAmount,
+    delivery_fee,
+    products,
+  } = useContext(ShopContext);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData((data) => ({ ...data, [name]: value }));
+  };
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const orderItems = [];
+
+      for (const items in cartItems) {
+        for (const item in cartItems[items]) {
+          if (cartItems[items][item] > 0) {
+            //it means that product have been added to the cart and the quantity is greateer than 0
+            const itemInfo = structuredClone(
+              products.find((product) => product._id === items) //using this structuredClone we can create 1 copy of any object in different variable
+            );
+            if (itemInfo) {
+              itemInfo.size = item;
+              itemInfo.quantity = cartItems[items][item];
+              orderItems.push(itemInfo);
+            }
+          }
+        }
+      }
+      // console.log(orderItems);
+      let orderData = {
+        address: formData,
+        items: orderItems,
+        amount: getCartAmount() + delivery_fee,
+      };
+
+      switch (method) {
+
+        //API calls for COD
+        case "cod":
+          const response = await axios.post(backendUrl+'/api/order/place',orderData,{headers:{token}})
+          // console.log(response.data)
+          if(response.data.success){
+            //it will clear the cart data and we'll navigate to the orders page
+            setCartItems({})
+            navigate('/orders')
+          }else{
+            toast.error(response.data.message)
+          }
+          break;
+
+        default:
+          break;
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  };
 
   return (
-    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t"
+    >
       {/* --------Left Side-------- */}
       <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
         <div className="text-xl sm:text-2xl my-3">
@@ -17,33 +100,57 @@ const PlaceOrder = () => {
         </div>
         <div className="flex gap-3">
           <input
+            required
+            onChange={onChangeHandler}
+            name="firstName"
+            value={formData.firstName}
             placeholder="First name"
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
             type="text"
           />
           <input
+            required
+            onChange={onChangeHandler}
+            name="lastName"
+            value={formData.lastName}
             placeholder="Last name"
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
             type="text"
           />
         </div>
         <input
+          required
+          onChange={onChangeHandler}
+          name="email"
+          value={formData.email}
           placeholder="Email address"
           className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
           type="email"
         />
         <input
+          required
+          onChange={onChangeHandler}
+          name="street"
+          value={formData.street}
           placeholder="Street"
           className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
           type="text"
         />
         <div className="flex gap-3">
           <input
+            required
+            onChange={onChangeHandler}
+            name="city"
+            value={formData.city}
             placeholder="City"
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
             type="text"
           />
           <input
+            required
+            onChange={onChangeHandler}
+            name="state"
+            value={formData.state}
             placeholder="State"
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
             type="text"
@@ -51,17 +158,29 @@ const PlaceOrder = () => {
         </div>
         <div className="flex gap-3">
           <input
+            required
+            onChange={onChangeHandler}
+            name="zipcode"
+            value={formData.zipcode}
             placeholder="Zipcode"
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
             type="number"
           />
           <input
+            required
+            onChange={onChangeHandler}
+            name="country"
+            value={formData.country}
             placeholder="Country"
             className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
             type="text"
           />
         </div>
         <input
+          required
+          onChange={onChangeHandler}
+          name="phone"
+          value={formData.phone}
           placeholder="Phone"
           className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
           type="number"
@@ -113,11 +232,16 @@ const PlaceOrder = () => {
             </div>
           </div>
           <div className="w-full text-end mt-8">
-            <button onClick={()=>navigate('/orders')} className="bg-black text-white px-16 py-3 text-sm">PLACE ORDER</button>
+            <button
+              type="submit"
+              className="bg-black text-white px-16 py-3 text-sm"
+            >
+              PLACE ORDER
+            </button>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
